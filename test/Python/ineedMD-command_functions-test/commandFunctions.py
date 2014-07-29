f2f90429093cb38c4165278407479f77001b244f
Matt B <mattb@oodg.com>
2014-07-29 13:04:55 -0700

    added cmnd fcns v3

--- test/Python/ineedMD-command_functions-test/commandFunctions.py
+++ test/Python/ineedMD-command_functions-test/commandFunctions.py
@@ -1,7 +1,7 @@
 import serial
 import time
 
-ser = serial.Serial(port ='/dev/tty.SerialPort', baudrate =115200)
+ser = serial.Serial(port ='/dev/tty.usbserial', baudrate =115200)
 print "opened port and running main()"
 
 ser.timeout = 1
@@ -41,10 +41,10 @@ def set_time(timeSec):
     if len(timeSec2) != 10:
         print "please recheck your time. should be represented in seconds since 1900"
     else:
-        hexTime = hex(timeSec)
-        ser.write(r'x9C\x01\x12\x12\x00\x00\x40\x00\x00\x'
-                  + hexTime [2:4] + r'\x'+ hexTime [4:6] + r'\x'+ hexTime [6:8] + r'\x'+ hexTime [8:10] + r'\x'
-                  + r'\x00\x00\x00\x00\xC9')
+        hexTime = hex(timeSec)[2:].decode('hex')
+        ser.write('\x9C\x01\x12\x12\x00\x00\x40\x00\x00')
+        ser.write(hexTime [0:2] + hexTime [2:4] + hexTime [4:6] + hexTime [6:8])
+        ser.write('\x00\x00\x00\x00\xC9')
         print "changing time " + hexTime
 
 def off_alarm():
@@ -62,15 +62,17 @@ def get_dataset_info():
 
 #4 DATA SET TRANSFER
 def transfer_dataset(datasetID):
-
-    if len(datasetID) != 4:
+    datastr = str(datasetID)
+    if len(datastr) != 4:
         print "not a valid data set ID"
     else:
         byte1 = datasetID [:2]
         byte2 = datasetID [2:]
-        strToSend = r'x9C\x01\x0A\x14\x00\x00\x00\x' + byte1 + r'\x' + byte2 + r'\xC9'
-        print "retrieving data set " + datasetID 
-        
+        ser.write('\x9C\x01\x0A\x14\x00\x00\x00')
+        ser.write(byte1.decode('hex'))
+        ser.write(byte2.decode('hex'))
+        ser.write('\xC9')
+
 #5 ERASE DATA SET
 def erase_dataset(datasetID):
     
@@ -79,8 +81,10 @@ def erase_dataset(datasetID):
     else:
         byte1 = datasetID [:2]
         byte2 = datasetID [2:]
-        strToSend = r'x9C\x01\x0A\x15\x00\x00\x00\x' + byte1 + r'\x' + byte2 + r'\xC9'
-        print "erasing data set " + datasetID 
+        ser.write('\x9C\x01\x0A\x15\x00\x00\x00')
+        ser.write(byte1.decode('hex'))
+        ser.write(byte2.decode('hex'))
+        ser.write('\xC9')
 
 
 #6 CAPTURE DATA SET FOR GIVEN TIME
@@ -99,32 +103,48 @@ def capture_data(seconds):
         byte1 = hexVal [:2]
         byte2 = hexVal [2:4]
         byte3 = hexVal [4:6]
-        strToSend = r'x9C\x01\x0A\x16\x00\x00\x'+ byte1 + r'\x' + byte2 + r'\x'+ byte3 + r'\xC9'
+        ser.write('\x9C\x01\x0A\x16\x00\x00')
+        ser.write(byte1.decode('hex'))
+        ser.write(byte2.decode('hex'))
+        ser.write(byte3.decode('hex'))
+        ser.write('\xC9')
         print "6"
     elif len(hexVal) == 5:
         byte1 = '0' + hexVal[:1]
         byte2 = hexVal [1:3]
         byte3 = hexVal [3:5]
-        strToSend = r'x9C\x01\x0A\x16\x00\x00\x'+ byte1 + r'\x' + byte2 + r'\x'+ byte3 + r'\xC9'
+        ser.write('\x9C\x01\x0A\x16\x00\x00')
+        ser.write(byte1.decode('hex'))
+        ser.write(byte2.decode('hex'))
+        ser.write(byte3.decode('hex'))
+        ser.write('\xC9')
         print "5"
     elif len(hexVal) == 4:
         byte1 = hexVal [:2]
         byte2 = hexVal [2:4]
-        strToSend = r'x9C\x01\x0A\x16\x00\x00\x00\x' + byte1 + r'\x'+ byte2 + r'\xC9'
+        ser.write('\x9C\x01\x0A\x16\x00\x00\x00')
+        ser.write(byte1.decode('hex'))
+        ser.write(byte2.decode('hex'))
+        ser.write('\xC9')
         print "4"
     elif len(hexVal) == 3:
         byte1= '0' + hexVal[:1]
         byte2 = hexVal[1:3]
-        strToSend = r'x9C\x01\x0A\x16\x00\x00\x00\x' + byte1 + r'\x'+ byte2 + r'\xC9'
+        ser.write('\x9C\x01\x0A\x16\x00\x00\x00')
+        ser.write(byte1.decode('hex'))
+        ser.write(byte2.decode('hex'))
+        ser.write('\xC9')
         print "3"
     elif len(hexVal) == 2:
-        strToSend = r'x9C\x01\x0A\x16\x00\x00\x00\x00\x' + hexVal + r'\xC9'
+        ser.write('\x9C\x01\x0A\x16\x00\x00\x00\x00')
+        ser.write(hexVal.decode('hex'))
+        ser.write('\xC9')
         print "2"
     elif len(hexVal) == 1:
-        strToSend = r'x9C\x01\x0A\x16\x00\x00\x00\x00\x0' + hexVal + r'\xC9'
+        ser.write('\x9C\x01\x0A\x16\x00\x00\x00\x00')
+        ser.write(hexVal)
+        ser.write('\xC9')
         print "1"
-
-
 
 #7 DISPLAY REAL TIME DATA MEASUREMENTS
 def get_rtData(TF):
@@ -145,34 +165,26 @@ def get_rtData(TF):
 def main():
     
     entry = raw_input('Enter any key... ')
-    #get_status()
+    get_status()
     
     set_pwr("hibernate")
     set_pwr("sleep")
     set_pwr("on")
     set_pwr("highPower")
-    #set_pwr("test wrong status request")
 
-    #set_time(3613939200)
-
-    #off_alarm()
-
+    set_time(3613939200)
+    off_alarm()
     get_dataset_info()
-
-    #transfer_dataset("A3B0")
-
-    #erase_dataset("11A0")
-
+    transfer_dataset("A3B0")
+    erase_dataset("11A0")
     get_rtData("T")
     get_rtData("F")
-    #get_rtData("A")
-
-    #capture_data(1)
-    #capture_data(28)
-    #capture_data(2949)
-    #capture_data(35785)
-    #capture_data(572570)
-    #capture_data(9161135)
+    capture_data(1)
+    capture_data(28)
+    capture_data(2949)
+    capture_data(35785)
+    capture_data(572570)
+    capture_data(9161135)
     
 
     exit = raw_input('Enter any key to exit... ')
