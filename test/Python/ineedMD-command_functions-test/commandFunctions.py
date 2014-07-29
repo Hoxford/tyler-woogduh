bfb0797f4d3b1804b9381a0a47e8a4ed70f30ca6
Matt B <mattb@oodg.com>
2014-07-29 13:08:18 -0700

    added cmnd fcns v8

--- test/Python/ineedMD-command_functions-test/commandFunctions.py
+++ test/Python/ineedMD-command_functions-test/commandFunctions.py
@@ -7,11 +7,12 @@ print "opened port and running main()"
 
 ser.timeout = 1
 
+
 #1 STATUS REQUEST
 def get_status():
     
     ser.write('\x9C\x01\x0A\x11\x00\x00\x00\x00\x00\xC9')
-    print "Get Status Command - 9C 01 0A 11 00 00 00 00 00 C9"
+    #print "Get Status Command - 9C 01 0A 11 00 00 00 00 00 C9"
     print "requesting status..."
 
 
@@ -27,8 +28,7 @@ def set_time(timeSec):
         ser.write('\x9C\x01\x12\x12\x00\x00\x40\x00\x00')
         ser.write(hexTime [0:2] + hexTime [2:4] + hexTime [4:6] + hexTime [6:8])
         ser.write('\x00\x00\x00\x00\xC9')
-        print "changing time " + hexTime
-        print "Setting time now!"
+        print "Setting time!"
         
         global timeSet
         timeSet = 'True'
@@ -94,7 +94,7 @@ def set_pwr(pwr):
 def off_alarm():
     if timeSet != 'True':
         ser.write('\x9C\x01\x12\x12\x00\x00\x60\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xC9')
-        print "Packet sent for True Condition - 9C 01 12 12 00 00 60 00 00 00 00 00 00 00 00 00 00 C9"
+        ##print "Packet sent for True Condition - 9C 01 12 12 00 00 60 00 00 00 00 00 00 00 00 00 00 C9"
         print"turning off all alarms; time 0"
     else:
         ser.write('\x9C\x01\x12\x12\x00\x00\x40\x00\x00')
@@ -108,7 +108,7 @@ def get_dataset_info():
     
     ser.write('\x9C\x01\x0A\x13\x00\x00\x00\x00\x00\xC9')
     print "requesting information of data set"
-    print "Packet sent - 9C 01 0A 13 00 00 00 00 00 C9"
+    ##print "Packet sent - 9C 01 0A 13 00 00 00 00 00 C9"
     
 
 #4 DATA SET TRANSFER
@@ -123,7 +123,8 @@ def transfer_dataset(datasetID):
         ser.write(byte1.decode('hex'))
         ser.write(byte2.decode('hex'))
         ser.write('\xC9')
-        print "Packet sent - 9C 01 0A 14 00 00 00 "+byte1+" "+byte2+" C9"
+        print "transfer data set"
+        ##print "Packet sent - 9C 01 0A 14 00 00 00 "+byte1+" "+byte2+" C9"
 
 #5 ERASE DATA SET
 def erase_dataset(datasetID):
@@ -137,7 +138,8 @@ def erase_dataset(datasetID):
         ser.write(byte1.decode('hex'))
         ser.write(byte2.decode('hex'))
         ser.write('\xC9')
-        print "Packet sent - 9C 01 0A 15 00 00 00 "+byte1+" "+byte2+" C9"
+        "erase data set"
+        ##print "Packet sent - 9C 01 0A 15 00 00 00 "+byte1+" "+byte2+" C9"
 
 #6 CAPTURE DATA SET FOR GIVEN TIME
 def capture_data(seconds):
@@ -196,7 +198,7 @@ def capture_data(seconds):
         ser.write('\x9C\x01\x0A\x16\x00\x00\x00\x00')
         ser.write(hexVal)
         ser.write('\xC9')
-        print "Packet sent - 9C 01 0A 16 00 00 00 00 31 C9"
+        ##print "Packet sent - 9C 01 0A 16 00 00 00 00 31 C9"
         print "1"
 
 #7 DISPLAY REAL TIME DATA MEASUREMENTS
@@ -205,14 +207,17 @@ def get_rtData(TF):
     if TF == "T":
         ser.write('\x9C\x01\x0A\x17\x00\x00\x00\x00\xFF\xC9')
         print "begin streaming of real time data"
-        print "Packet sent - 9C 01 0A 17 00 00 00 00 FF C9"
+        #print "Packet sent - 9C 01 0A 17 00 00 00 00 FF C9"
     elif TF =="F":
         ser.write('\x9C\x01\x0A\x17\x00\x00\x00\x00\x00\xC9')
         print "do not stream real time data"
-        print "Packet sent - 9C 01 0A 17 00 00 00 00 00 C9"
+        #print "Packet sent - 9C 01 0A 17 00 00 00 00 00 C9"
     else:
         ser.write('\x9C\x01\x0A\x17\x00\x00\x09\xC9')
         print "not a valid input for this function"
+
+
+
 
 
 
@@ -222,117 +227,143 @@ def main():
     timeSet = 'False'   
 
     ser.flushInput()
+    entry = raw_input('Enter any key... ')
+    i = 0
     
-    entry = raw_input('Enter any key... ')
-    set_time(3613939200)
-    read = raw_input("Enter any key to read .. ")
-    print "DATA READ: " + ser.readline()
+    while i < 10:
+        
+            set_time(3613939200)
+            time.sleep(2) 
+            if ser.inWaiting() != 0:
+                print "RESPONSE RECEIVED: " + ser.readline()
+                print "STUFF IS IN THE BUFFER. DO YOU SEE???"
+                ser.readline()
+                
+            get_status() #0x11
+            time.sleep(2)
+            if ser.inWaiting() != 0:
+                print "RESPONSE RECEIVED: " + ser.readline()
+                ser.readline()
 
-    entry = raw_input('Enter any key... ')
-    get_status() #0x11
-    read = raw_input("Enter any key to read .. ")
-    print "DATA READ: " + ser.readline()
+            capture_data(1) #0x16
+            time.sleep(2)
+            if ser.inWaiting() != 0:
+                print "RESPONSE RECEIVED: " + ser.readline()
+                ser.readline()
 
-    entry = raw_input('Enter any key... ')
-    capture_data(1) #0x16
-    read = raw_input("Enter any key to read .. ")
-    print "DATA READ: " + ser.readline()
-
-    entry = raw_input('Enter any key... ')
-    off_alarm()#0x12
-    read = raw_input("Enter any key to read .. ")
-    print "DATA READ: " + ser.readline()
-
-    entry = raw_input('Enter any key... ')
-    set_pwr("hibernate")
-    read = raw_input("Enter any key to read .. ")
-    print "DATA READ: " + ser.readline()
-
-    entry = raw_input('Enter any key... ')
-    set_pwr("sleep")
-    read = raw_input("Enter any key to read .. ")
-    print "DATA READ: " + ser.readline()
-
-    entry = raw_input('Enter any key... ')
-    set_pwr("highPower")
-    read = raw_input("Enter any key to read .. ")
-    print "DATA READ: " + ser.readline()
-
-    entry = raw_input('Enter any key... ')
-    set_pwr("on")
-    read = raw_input("Enter any key to read .. ")
-    print "DATA READ: " + ser.readline()
-
-    entry = raw_input('Enter aney key... ')
-    get_dataset_info() #0x13
-    read = raw_input("Enter any key to read .. ")
-    print "DATA READ: " + ser.readline()
-
-    entry = raw_input('Enter any key... ')
-    transfer_dataset("A3B0")
-    read = raw_input("Enter any key to read .. ")
-    print "DATA READ: " + ser.readline()
+            off_alarm()#0x12
+            time.sleep(2)
+            if ser.inWaiting() != 0:
+                print "RESPONSE RECEIVED: " + ser.readline()
+                ser.readline()
  
-    entry = raw_input('Enter any key... ')
-    erase_dataset("11A0")
-    read = raw_input("Enter any key to read .. ")
-    print "DATA READ: " + ser.readline()
+            set_pwr("hibernate")
+            time.sleep(2)
+            if ser.inWaiting() != 0:
+                print "RESPONSE RECEIVED: " + ser.readline()
+                ser.readline()
 
-    entry = raw_input('Enter any key... ')
-    get_rtData("T")
-    read = raw_input("Enter any key to read .. ")
-    print "DATA READ: " + ser.readline()
+            set_pwr("sleep")
+            time.sleep(2)
+            if ser.inWaiting() != 0:
+                print "RESPONSE RECEIVED: " + ser.readline()
+                ser.readline()
 
-    entry = raw_input('Enter any key... ')
-    get_rtData("F")
-    read = raw_input("Enter any key to read .. ")
-    print "DATA READ: " + ser.readline()
-
-    entry = raw_input('Enter any key... ')
-    capture_data(28)
-    read = raw_input("Enter any key to read .. ")
-    print "DATA READ: " + ser.readline()
-
-    entry = raw_input('Enter any key... ')
-    capture_data(2949)
-    read = raw_input("Enter any key to read .. ")
-    print "DATA READ: " + ser.readline()
-
-    entry = raw_input('Enter any key... ')
-    capture_data(35785)
-    read = raw_input("Enter any key to read .. ")
-    print "DATA READ: " + ser.readline()
-
-    entry = raw_input('Enter any key... ')
-    capture_data(572570)
-    read = raw_input("Enter any key to read .. ")
-    print "DATA READ: " + ser.readline()
-
-    entry = raw_input('Enter any key... ')
-    capture_data(9161135)
-    read = raw_input("Enter any key to read .. ")
-    print "DATA READ: " + ser.readline()
-
-    entry = raw_input('Enter any key... ')
-    ser.write('helloworld')
-    read = raw_input("Enter any key to read .. ")
-    print "DATA READ: " + ser.readline()
- 
-    
- 
- 
-   
-   
- 
-    
- 
+            set_pwr("highPower")
+            time.sleep(2)
+            if ser.inWaiting() != 0:
+                print "RESPONSE RECEIVED: " + ser.readline()
+                ser.readline()
   
+            set_pwr("on")
+            time.sleep(2)
+            if ser.inWaiting() != 0:
+                print "RESPONSE RECEIVED: " + ser.readline()
+                ser.readline()
 
+            get_dataset_info() #0x13
+            time.sleep(2)
+            if ser.inWaiting() != 0:
+                print "RESPONSE RECEIVED: " + ser.readline()
+                ser.readline()
+
+            transfer_dataset("A3B0")
+            time.sleep(2)
+            if ser.inWaiting() != 0:
+                print "RESPONSE RECEIVED: " + ser.readline()
+                ser.readline()
+
+            erase_dataset("11A0")
+            time.sleep(2)
+            if ser.inWaiting() != 0:
+                print "RESPONSE RECEIVED: " + ser.readline()
+                ser.readline()
  
+##            get_rtData("T")
+##            time.sleep(2)
+##            if ser.inWaiting() != 0:
+##
+##                print "RESPONSE RECEIVED: " + ser.readline()
+##                ser.readline()
+
+            get_rtData("F")
+            time.sleep(2)
+            if ser.inWaiting() != 0:
+ 
+                print "RESPONSE RECEIVED: " + ser.readline()
+                ser.readline()
+
+            capture_data(28)
+            time.sleep(2)
+            if ser.inWaiting() != 0:
+ 
+                print "RESPONSE RECEIVED: " + ser.readline()
+                ser.readline()
   
-    
+            capture_data(2949)
+            time.sleep(2)
+            if ser.inWaiting() != 0:
    
+                print "RESPONSE RECEIVED: " + ser.readline()
+                ser.readline()
+
+            capture_data(35785)
+            time.sleep(2)
+            if ser.inWaiting() != 0:
+
+                print "RESPONSE RECEIVED: " + ser.readline()
+                ser.readline()
+
+            capture_data(572570)
+            time.sleep(2)
+            if ser.inWaiting() != 0:
+
+                print "RESPONSE RECEIVED: " + ser.readline()
+                ser.readline()
   
+            capture_data(9161135)
+            time.sleep(2)
+            if ser.inWaiting() != 0:
+  
+                print "RESPONSE RECEIVED: " + ser.readline()
+                ser.readline()
+ 
+            ser.write('helloworld')
+            time.sleep(2)
+            if ser.inWaiting() != 0:
+       
+                print "RESPONSE RECEIVED: " + ser.readline()
+                ser.readline()
+
+            ser.write('\x9C\x01\x0A\x16\x00\x00\x00\x00')
+            time.sleep(2)
+            if ser.inWaiting() != 0:
+       
+                print "RESPONSE RECEIVED: " + ser.readline()
+                ser.readline()
+
+            i += 1
+
   
     #ser.flushInput()
     exit = raw_input('Enter any key to exit... ')
