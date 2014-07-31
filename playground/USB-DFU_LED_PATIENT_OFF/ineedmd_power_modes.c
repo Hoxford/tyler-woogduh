872ed40eed4c8edcd957518a3c0e5a0ab563ad74
unknown <BenS@WIN-2B2VVUDG414.kymeta.local>
2014-07-30 18:45:23 -0700

    restored ineedmd power modes

--- playground/USB-DFU_LED_PATIENT_OFF/ineedmd_power_modes.c
+++ playground/USB-DFU_LED_PATIENT_OFF/ineedmd_power_modes.c
@@ -40,30 +40,30 @@
 void
 go_to_sleep(int number_tenths_seconds){
 
-	debug_printf("going to sleep");
-	// power down the radio
-	if (iRadio_Power_Off()==1)
-	{
-		debug_printf("..radio asleep");
-	}
-	// power down the ADC
-	if (EKGSPIDisable()==1)
-	{
-		debug_printf("..EKG ADC asleep");
-	}
-	// led's off  controller powers itself down 5ms after LEDS are off
-	ineedmd_led_pattern(LED_OFF);
-	LEDI2CDisable();
-	// power down ADC
+        debug_printf("going to sleep");
+        // power down the radio
+        if (iRadio_Power_Off()==1)
+        {
+                debug_printf("..radio asleep");
+        }
+        // power down the ADC
+        if (EKGSPIDisable()==1)
+        {
+                debug_printf("..EKG ADC asleep");
+        }
+        // led's off  controller powers itself down 5ms after LEDS are off
+        ineedmd_led_pattern(LED_OFF);
+        LEDI2CDisable();
+        // power down ADC
 
-	//if (BatMeasureADCDisable()==1)
-	//{
-	//	debug_printf("..Battery measurement asleep");
-	//}
+        if (BatMeasureADCDisable()==1)
+        {
+                debug_printf("..Battery measurement asleep");
+        }
 
-	USBPortDisable();
+        USBPortDisable();
 
-	// start timer
+        // start timer
     //
     // Set the Timer0B load value to 10s.
     //
@@ -89,54 +89,54 @@ go_to_sleep(int number_tenths_seconds){
     // clocks down the processor to REALLY slow ( 500khz) and
     //
     // go to a slow clock
-	if (set_system_speed (INEEDMD_CPU_SPEED_SLOW_INTERNAL) == INEEDMD_CPU_SPEED_SLOW_INTERNAL)
-	{
-		debug_printf("..CPU slow");
+        if (set_system_speed (INEEDMD_CPU_SPEED_SLOW_INTERNAL) == INEEDMD_CPU_SPEED_SLOW_INTERNAL)
+        {
+                debug_printf("..CPU slow");
 
-	}
+        }
     //
     // Enable Timer0(A)
     //
     TimerEnable(TIMER0_BASE, TIMER_A);
 
-	// and deep sleep.
-    ROM_SysCtlDeepSleep();
+        // and deep sleep.
+        ROM_SysCtlDeepSleep();
 
-	    TimerDisable(TIMER0_BASE, TIMER_A);
-	    IntDisable(INT_TIMER0A);
-	    IntMasterDisable();
+            TimerDisable(TIMER0_BASE, TIMER_A);
+            IntDisable(INT_TIMER0A);
+            IntMasterDisable();
 
 }
 
 void
 wake_up(void){
 
-	debug_printf("waking_up");
-	//go to a fast clock
-    	    // go to a slow clock
-    		if(set_system_speed(INEEDMD_CPU_SPEED_HALF_INTERNAL) == INEEDMD_CPU_SPEED_HALF_INTERNAL)
-    		{
-    			debug_printf("..CPU half speed");
+        debug_printf("waking_up");
+        //go to a fast clock
+            // go to a slow clock
+                if(set_system_speed(INEEDMD_CPU_SPEED_HALF_INTERNAL) == INEEDMD_CPU_SPEED_HALF_INTERNAL)
+                {
+                        debug_printf("..CPU half speed");
 
-    		}
+                }
 
-	if (RadioUARTEnable()==1)
-	{
-		debug_printf("..radio awake");
-	}
-	// power down the ADC
-	if (EKGSPIEnable()==1)
-	{
-		debug_printf("..EKG ADC awake");
-	}
-	// enable the I2C bus
-	LEDI2CEnable();
+        if (RadioUARTEnable()==1)
+        {
+                debug_printf("..radio awake");
+        }
+        // power down the ADC
+        if (EKGSPIEnable()==1)
+        {
+                debug_printf("..EKG ADC awake");
+        }
+        // enable the I2C bus
+        LEDI2CEnable();
 
-	USBPortEnable();
+        USBPortEnable();
 
-//	if (BatMeasureADCEnable()==1)
-//	{
-//		debug_printf("..Battery measurement asleep");
-//	}
+        if (BatMeasureADCEnable()==1)
+        {
+                debug_printf("..Battery measurement asleep");
+        }
 
 }
