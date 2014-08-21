317c5fb9b29394625d1602df373e4cace7676bfa
unknown <BenS@WIN-2B2VVUDG414.kymeta.local>
2014-08-21 08:23:17 -0700

    updated command protocol updated led driver to match document

--- application/app_inc/ineedmd_command_protocol.h
+++ application/app_inc/ineedmd_command_protocol.h
@@ -14,7 +14,31 @@
 //*****************************************************************************
 // defines
 //*****************************************************************************
+#define INMD_FRAME_BUFF_SIZE    256
+#define NACK_FRAME_SIZE  12
+#define ACK_FRAME_SIZE   12
+//#define DWORD    unsigned long
 
+#define COMMAND_FLAG      0x01
+#define STATUS_FLAG       0x02
+#define MEASURE_FLAG      0x03
+#define STATUS_HIBERNATE  0x00
+#define STATUS_SLEEP      0x20
+#define STATUS_ON         0x40
+#define STATUS_POWER      0x60
+
+#define REQUEST_TO_TEST   0x18
+
+#define STOP_EKG_TEST_PAT 0x00
+
+#define EKG_TEST_PAT      0x01
+#define TRIANGLE_TEST_PAT 0x00
+#define SQUARE_TEST_PAT   0x01
+#define WAVEFORM_TEST_PAT 0x02
+
+#define LED_TEST_PATTERN  0x02
+#define REQ_FOR_DFU       0x03
+#define REQ_FOR_RESET     0x04
 //*****************************************************************************
 // variables
 //*****************************************************************************
